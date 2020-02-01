7d34e10c4f14cbf472d34ba9410f8f2861b10985
thufir <hawat.thufir@gmail.com>
2020-02-01 01:36:44 -0800

    incremental

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -11,8 +11,10 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.basex.build.json.JsonParser;
+import org.basex.core.MainOptions;
+import org.basex.io.IOFile;
 import twitter4j.JSONException;
 import twitter4j.JSONObject;
 import twitter4j.TwitterException;
@@ -23,41 +25,44 @@ public class App {
     private static final Logger log = Logger.getLogger(App.class.getName());
 
     private void baseX(List<JSONObject> tweets) throws MalformedURLException, IOException {
-        //    TwitterOps to = new TwitterOps(loadProperties("twitter"));
         Properties databaseProperties = new Properties();
         databaseProperties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
         DatabaseOps db = new DatabaseOps(databaseProperties);
-        //  db.fetch();
         db.addTweets(tweets);
     }
 
-    private List<JSONObject> getTweets() {
+    private List<JSONObject> getTweets() throws TwitterException, IOException, JSONException {
         List<JSONObject> tweets = new ArrayList<>();
-        try {
-            tweets = new TwitterOps().getTweets();
-        } catch (TwitterException | IOException | JSONException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        log.info("tweets\t\t" + tweets.size());
+        tweets = new TwitterOps().getTweets();
+        log.fine("tweets\t\t" + tweets.size());
         return tweets;
     }
 
     private void twitterToBaseX() throws UnsupportedEncodingException, IOException {
         List<JSONObject> tweets = getTweets();
-        log.fine(tweets.toString());
-        convertJsonToXml("tweets.json", tweets);
-        baseX(tweets);
-
+        String fileName = "tweets.json";
+        writeJsonToFile(fileName, tweets);
+        baseX(fileName);
     }
 
-    private void convertJsonToXml(String fileName, List<JSONObject> tweets) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+    private void writeJsonToFile(String fileName, List<JSONObject> tweets) throws FileNotFoundException, UnsupportedEncodingException, IOException {
         FileOutputStream fileOutputStream = null;
         OutputStreamWriter outputStreamWriter = null;
-        BufferedWriter BufferedWriter = null;
+        BufferedWriter bufferedWriter = null;
         fileOutputStream = new FileOutputStream(fileName);
         outputStreamWriter = new OutputStreamWriter(fileOutputStream, "UTF-8");
-        BufferedWriter = new BufferedWriter(outputStreamWriter);
-        BufferedWriter.write(tweets.toString());
+        bufferedWriter = new BufferedWriter(outputStreamWriter);
+        bufferedWriter.write(tweets.toString());
+        bufferedWriter.close();
+        outputStreamWriter.close();
+        fileOutputStream.close();
+    }
+
+    private void baseX(String fileName) throws IOException {
+        IOFile iof = new IOFile(fileName);
+        MainOptions opts = new MainOptions();
+        JsonParser JsonParser = new JsonParser(iof, opts);
+        log.info("file has been read...");
     }
 
     public static void main(String... args) throws IOException {
