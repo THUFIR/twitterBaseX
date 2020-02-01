3451860c359282fbe9ce941b3f6a5b8bbbe9fae8
thufir <hawat.thufir@gmail.com>
2020-01-31 22:38:19 -0800

    incremental

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -3,8 +3,10 @@ package main;
 import basex.DatabaseOps;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.basex.core.BaseXException;
 import twitter4j.JSONException;
@@ -24,14 +26,22 @@ public class App {
         db.fetch();
     }
 
-    private void twitter() throws TwitterException, IOException, JSONException {
+    private List<JSONObject> getTweets() throws TwitterException, IOException, JSONException {
         List<JSONObject> tweets = new TwitterOps().getTweets();
+        return tweets;
     }
 
     private void init() {
+        List<JSONObject> tweets = new ArrayList<>();
+        try {
+            tweets = getTweets();
+        } catch (TwitterException | IOException | JSONException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        log.info(tweets.toString());
     }
 
-    public static void main(String... args) throws BaseXException, IOException, MalformedURLException {
+    public static void main(String... args)  {
         new App().init();
     }
 
