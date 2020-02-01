79b77f1df68a79123c9c9714b6185eaafca751fd
thufir <hawat.thufir@gmail.com>
2020-02-01 01:58:20 -0800

    incremental

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,6 +2,7 @@ package main;
 
 import basex.DatabaseOps;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,6 +15,7 @@ import java.util.Properties;
 import java.util.logging.Logger;
 import org.basex.build.json.JsonParser;
 import org.basex.build.xml.SAXWrapper;
+import org.basex.core.BaseXException;
 import org.basex.core.MainOptions;
 import org.basex.io.IOFile;
 import twitter4j.JSONException;
@@ -52,13 +54,11 @@ public class App {
         fileOutputStream.close();
     }
 
-
-    private void baseX(List<JSONObject> tweets, fileName) throws MalformedURLException, IOException {
+    private void baseX(String fileName) throws MalformedURLException, BaseXException, IOException {
         Properties databaseProperties = new Properties();
         databaseProperties.loadFromXML(App.class.getResourceAsStream("/basex.xml"));
         DatabaseOps db = new DatabaseOps(databaseProperties);
         JsonParser jsonParser = new JsonParser(new IOFile(fileName), new MainOptions());
-        db.addTweets(tweets);
     }
 
     public static void main(String... args) throws IOException, UnsupportedEncodingException, TwitterException, JSONException {
